314af65dd12525e8d1fc75f5516ca8e05b6503b6
Amit Kumar <amitmaurya42@gmail.com>
2021-01-11 14:10:27 +0530

    Add validation.

--- src/container/Login.js
+++ src/container/Login.js
@@ -2,10 +2,32 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import {connect} from 'react-redux';
 import { loginHandler } from "../Actions/AuthAction";
+import ReactFormInputValidation from "react-form-input-validation";
 
 class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            fields:{
+                email:'',
+                password:''
+            },
+            errors:{}
+        }
+        this.form =  new ReactFormInputValidation(this);
+        this.form.useRules({
+            email:'required|email',
+            password:'required|digits_between:1,8'
+        })
+        this.form.onformsubmit = (fields)=>{
+            this.props.loginHandler(fields.email,fields.password)
+        }
+    };
+    
+    
     render(){
         return(
+            <React.Fragment>
             <div className="col-sm-8">     
                 <div className="form-box">
                     <div className="form-top">
@@ -20,22 +42,34 @@ class Login extends React.Component{
                         </div>
                     </div>
                     <div className="form-bottom">
-                        <form role="form" method="post" className="login-form">
+                        <form method="post" onSubmit={this.form.handleSubmit} className="login-form">
                             <div className="form-group">
                                 <label className="sr-only">Username</label>
-                                <input type="text" name="form-username" placeholder="Username..." className="form-username form-control" id="form-username" />
+                                <input type="text" name="email" placeholder="email..." className="form-username form-control" 
+                                onBlur={this.form.handleBlurEvent}
+                                onChange={this.form.handleChangeEvent}
+                                value={this.state.fields.email} 
+                                />
+                                <label className="error">
+                                    {this.state.errors.email ? this.state.errors.email : ""}
+                                </label>
                             </div>
                             <div className="form-group">
                                 <label className="sr-only">Password</label>
-                                <input type="password" name="form-password" placeholder="Password..." className="form-password form-control" id="form-password" />
+                                <input type="password" name="password" placeholder="Password..." className="form-password form-control" onBlur={this.form.handleBlurEvent}
+                                onChange={this.form.handleChangeEvent}
+                                value={this.state.fields.password}
+                                />
+                                <label className="error">
+                                    {this.state.errors.password ? this.state.errors.password : ""}
+                                </label>
                             </div>
-                            <button type="button" className="btn" onClick={()=>this.props.loginHandler('amitmaurya42@gmail.com','Chetu@123')}>Sign in!</button>
+                            <button type="submit" className="btn">Sign in!</button>
                         </form>
                     </div>
                 </div>
-            
-                
             </div>
+            </React.Fragment>
         )
     }
 }
